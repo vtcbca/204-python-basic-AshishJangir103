e6a6c7ca9b7179009787e359bcded28c4ddf5b76
AshishJangir103 <96675170+AshishJangir103@users.noreply.github.com>
2022-03-13 10:28:46 +0530

    Delete solution directory

--- solution/Solution 2.py
+++
@@ -1,7 +0,0 @@
-#WAS to enter kilometer and converted it into meter.
-#Name : Laxman Sirvi
-#Date : 09-03-22
-km=float(input("Enter a value of kilometer : "))
-meter=1000
-a=km*meter
-print("{} Km = {} meter".format(km,a))
--- solution/Solution 8.py
+++
@@ -1,8 +0,0 @@
-#WAS to print the sum of  1 to n number
-#Name : Laxman Sirvi
-#Date : 09-03-22
-n=int(input("Enter any No. to print the sum of N No. : "))
-sum=0
-for a in range(1,n+1):
-    sum=sum+a
-print("The sum of N No. is ",sum)
